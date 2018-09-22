739ef9042da2c04f64628d7ce02fd6da26a29e88
Patrick Morales <pamorales3@miners.utep.edu>
2018-09-22 16:24:25 -0600

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,45 @@
+// Checks off specific todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+    /*
+    if($(this).css("color") === "rgb(128, 128, 128)"){
+        $(this).css("color", "black");
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    } else {
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+    */
+});
+
+
+// Deletes a todo when the trash icon is clicked
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+
+// Creates a new todo 
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+
+        // create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+// Add and removes the text input box
+$(".fa-pencil-alt").click(function(){
+    $("input[type='text']").fadeToggle();
+});
